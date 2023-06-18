429a77f8ee7fd2f929034da6a5b043a931c79765
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-18 08:42:44 +0600

    first

---
+++ js/prompt.js
@@ -0,0 +1,9 @@
+
+
+const setPrompt = () => {
+  const myPrompt = prompt('enter a number')
+  console.log(myPrompt);
+  const getPromptNumber = parseFloat(myPrompt);
+  alert(getPromptNumber + 200);
+//   console.log(getPromptNumber);
+}
---
+++ js/set-time-out.js
@@ -0,0 +1,7 @@
+console.log('first');
+console.log('second');
+console.log('fourth');
+console.log('fifth');
+setTimeout(() => {
+    console.log('third');
+}, 3500);
